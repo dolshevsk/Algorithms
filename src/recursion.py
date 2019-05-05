fe196dbbe8147618c83c5a4f65a2390deba0bda5
dolshevsk <olshevskiydk@gmail.com>
2019-05-05 16:07:25 +0300

    some recursion practice

--- src/recursion.py
+++ src/recursion.py
@@ -13,7 +13,20 @@ def length_2(array, acc=0):
         acc +=1 
         return length_2(array[1:],acc)
 
+def sum(array):
+    if array == []:
+        return 0
+    return array[0] + sum(array[1:])
 
-my_list = [1,2,3,4,5,6,7]
+def max_val(array):
+    if len(array)==1:
+        return array[0]
+    else:
+        return max_val(array[1:]) if array[0]<=array[1] else max_val(array[:1] + array[2:])
+
+
+my_list = [1,2,3,4,15,6,7]
 print(length_1(my_list))
 print(length_2(my_list))
+print(sum(my_list))
+print(max_val(my_list))
