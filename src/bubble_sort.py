4304a727cc1f79b9f74785cf5d368e27c6aed728
dolshevsk <olshevskiydk@gmail.com>
2019-05-06 21:15:09 +0300

    add bubble_sort

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,3 +1,13 @@
 def bubble_sort(array):
-    while True:
-        if 
+    swaped = True
+    while swaped:
+        swaped = False
+        for i in range(len(array)-1):
+                if array[i] < array[i+1]:
+                        array[i], array[i+1]  = array[i+1], array[i]
+                        swaped = True
+    return array
+
+my_list = [4,5,6,23,12,4,31,1,0]
+print(bubble_sort(my_list))
+        
