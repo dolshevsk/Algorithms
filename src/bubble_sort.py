480ccda8fa7c70be2ddccc45e2c67189bb0bae21
dolshevsk <olshevskiydk@gmail.com>
2019-05-07 02:06:22 +0300

    add coctail_sort

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -3,11 +3,33 @@ def bubble_sort(array):
     while swaped:
         swaped = False
         for i in range(len(array)-1):
-                if array[i] < array[i+1]:
-                        array[i], array[i+1]  = array[i+1], array[i]
-                        swaped = True
+            if array[i] < array[i+1]:
+                array[i], array[i+1]  = array[i+1], array[i]
+                swaped = True
     return array
 
+def cocktail_sort(array):
+    left = 0
+    right = len(array)-1
+
+    while left <= right:
+        for i in range(left, right,+1):
+            if array[i] < array[i+1]:
+                 array[i], array[i+1] = array[i+1], array[i]
+        right -=1
+        print(f"after right -1 {array}")
+
+        for i in range(right, left, -1):
+            if array[i] > array[i-1]:
+                 array[i], array[i-1] = array[i-1], array[i]
+        left +=1
+        print(f"after left +1 {array}")
+    return array
+
+
+
 my_list = [4,5,6,23,12,4,31,1,0]
+my_list_2 = [4,5,6,23,12,4,31,1,0]
 print(bubble_sort(my_list))
+print(cocktail_sort(my_list_2))
         
