b2b74f51877706b6608452c0e1cb31bcf2f00e73
dolshevsk <olshevskiydk@gmail.com>
2019-05-06 20:25:27 +0300

    modifed selecting_sort

--- src/selecting_sort.py
+++ src/selecting_sort.py
@@ -10,6 +10,21 @@ def selecting_sort(array):
         sorted_list.append(array.pop(max_index))
     return sorted_list
 
+# without creating an empty list
+def selecting_sort_2(array):
+    for start_index in range(len(array)):
+        max = array[start_index]
+        for i in range(start_index +1, len(array)):
+            if  array[i] > max:
+                max = array[i]
+                max_index = i
+        array[start_index], array[max_index]= array[max_index], array[start_index]
+    return array
+                
+
 my_list = [4,5,6,23,12,4,31,1,0]
+my_list2 = [4,5,6,23,12,4,31,1,0]
 print(selecting_sort(my_list))
+print(my_list)
+print(selecting_sort_2(my_list2))
 
