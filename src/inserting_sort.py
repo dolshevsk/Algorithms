b8bacebd06ac139e68aa27edb4974b3bc8c58d03
dolshevsk <olshevskiydk@gmail.com>
2019-05-15 02:48:16 +0300

    update:make inserting sort much better

--- src/inserting_sort.py
+++ src/inserting_sort.py
@@ -5,12 +5,13 @@ def insertion_sort(array):
     total = 0
     for i in range(1, len(array)):
         item_to_insert = array[i]
-        
-        while i > 0 and array[i-1] < item_to_insert:
-            array[i] = array[i-1]
+        insert_index = i
+
+        while i > 0 and array[i-1] > item_to_insert:
             i -= 1
             total +=1
-        array[i]= item_to_insert
+        array = array[:i] + [item_to_insert] + array[i:insert_index] + array[insert_index+1:]
+       # array[i]= item_to_insert
     print(f'make {total} totals')
     return array
 
